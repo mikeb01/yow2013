fff75818b102ba39d618fcd63052b2396aa6217e
Michael Barker <michael.barker@lmax.com>
2013-09-19 12:48:09 +0000

    Update gc parsing shell script

--- runme.sh
+++ runme.sh
@@ -3,22 +3,22 @@
 rm *-gc.log
 
 echo "Running Simple..."
-java -Xloggc:simple-gc.log \
+$JAVA_HOME/bin/java -Xloggc:simple-gc.log \
      -verbose:gc \
      -XX:+PrintGCDateStamps \
      -XX:+PrintGCApplicationStoppedTime \
      -cp bin:lib/disruptor-3.2.0.jar yow2013.immutable.SimplePerformanceTest
 echo "Done"
 
-grep 'stopped:' simple-gc.log | cut -d':' -f 2 | cut -d' ' -f 2 | sort -n | awk '{ printf "%1.3f\n", $1 }' | (echo " Count Millis" ; uniq -c )
+grep 'stopped:' simple-gc.log | sed 's/.*stopped: \([0-9.]*\) seconds/\1/' | sort -n | awk '{ printf "%1.3f\n", $1 }' | (echo " Count Millis" ; uniq -c )
      
 echo "Running Custom..."
-java -Xloggc:custom-gc.log \
+$JAVA_HOME/bin/java -Xloggc:custom-gc.log \
      -verbose:gc \
      -XX:+PrintGCDateStamps \
      -XX:+PrintGCApplicationStoppedTime \
      -cp bin:lib/disruptor-3.2.0.jar yow2013.immutable.CustomPerformanceTest
 echo "Done"
 
-grep 'stopped:' custom-gc.log | cut -d':' -f 2 | cut -d' ' -f 2 | sort -n | awk '{ printf "%1.3f\n", $1 }' | (echo " Count Millis" ; uniq -c )
+grep 'stopped:' custom-gc.log | sed 's/.*stopped: \([0-9.]*\) seconds/\1/' | sort -n | awk '{ printf "%1.3f\n", $1 }' | (echo " Count Millis" ; uniq -c )
      
